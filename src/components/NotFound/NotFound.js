166e0127ed20b0525a25707a21bd9a92444acbe5
Rabbi01521 <ishan0563@gmail.com>
2021-11-13 21:01:18 +0600

    404 uploaded

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,33 @@
-import { Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <Typography variant="h1">404</Typography>
-    </div>
+    <Box sx={{ backgroundColor: "black" }}>
+      <Container
+        sx={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        <Box>
+          <Typography variant="h1">404</Typography>
+          <NavLink
+            style={{ color: "white", textDecoration: "none" }}
+            to="/home"
+          >
+            <Button variant="contained">Go Back To Home</Button>
+          </NavLink>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
